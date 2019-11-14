10f0f3faf2b25a3ee3a808d43e61bd91156bfbb8
FauxRedTester <cogent.redtester@outlook.com>
2019-11-14 16:25:42 +1030

    modified osc to allow for dynamic option updating during runtime

--- disabled scripts/audio-osc.lua
+++ disabled scripts/audio-osc.lua
@@ -2,12 +2,11 @@
 
 mp.register_event("file-loaded", function()
     local hasvid = mp.get_property_osd("video") ~= "no"
-    hasvid = false
+    --hasvid = false
     mp.commandv("script-message", "osc-visibility", (hasvid and "auto" or "always"), "no-osd")
     -- remove the next line if you don't want to affect the osd-bar config
     mp.commandv("set", "options/osd-bar", (hasvid and "yes" or "no"))
 
-    print(mp.get_property("options/script-opts"))
-
-    mp.commandv("set", "options/osc-boxvideo", "yes")
+    mp.commandv("apply-profile", "music")
+    mp.commandv("script-message", "update-options")
 end)
