a7a06727f93da9e589c12f1d292624b7ded60f2b
CogentRedTester <cogent.redtester@outlook.com>
2019-12-20 00:18:56 +1030

    fixed music-mode to run the undo profile and improved commenting.
    Removed the script message to update the osc, now the osc does so itself: https://github.com/CogentRedTester/my-mpv-settings/commit/9db31964b9583b602771abd5ded49530b620a5cd

--- music-mode.lua
+++ music-mode.lua
@@ -5,9 +5,14 @@ msg = require 'mp.msg'
 opt = require 'mp.options'
 
 o = {
+    --valid extensions to run music-mode
     exts = 'mp3;wav;ogm;flac;m4a;wma;ogg;opus;alac;mka',
+
+    --profile to call when valid extension is found
     profile = "music",
 
+    --runs this profile when in music mode and a non-audio file is loaded
+    --you should essentially put all your defaults that the music profile changed in here
     undo_profile = ""
 }
 
@@ -34,12 +39,14 @@ end
 
 --stores a table of the extensions
 local exts = split(o.exts)
+
+--to prevent superfluous loading of profiles the script keeps track of when music mode is enabled
 local inMusicMode = false
 
 function main()
     --finds the filename
     local filename = mp.get_property('filename')
-    msg.verbose(filename .. 'loaded')
+    msg.verbose('"' .. filename .. '" loaded')
     
     --finds the file extension
     local index = string.find(filename, [[.[^.]*$]])
@@ -48,17 +55,17 @@ function main()
 
     --if the extension is a valid audio extension then it switches to music mode
     if inTable(ext, exts) and inMusicMode == false then
-        msg.verbose('extension in whitelist, applying profiles')
+        msg.verbose('extension in whitelist, applying profile "' .. o.profile .. '"')
         mp.commandv('apply-profile', o.profile)
-        mp.commandv('script-message', 'update-osc-options')
 
         inMusicMode = true
-    elseif inMusicMode then
-        msg.verbose('extension not in whitelist, applying undo profiles')
+    elseif inMusicMode and o.undo_profile ~= "" then
+        msg.verbose('extension not in whitelist, applying undo profile "' .. o.undo_profile .. '"')
         mp.commandv('apply-profile', o.undo_profile)
-        mp.commandv('script-message', 'update-osc-options')
 
         inMusicMode = false
+    else
+        msg.verbose('extension not in whitelist, doing nothing')
     end
 end
 
