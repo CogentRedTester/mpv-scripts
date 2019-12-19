91008254c88e120cd51f2c3d891975cdaf06ffb4
Oscar Manglaras <cogent.redtester@outlook.com>
2019-12-19 23:22:10 +1030

    made initial changes to auto revert music mode

--- music-mode.lua
+++ music-mode.lua
@@ -6,7 +6,9 @@ opt = require 'mp.options'
 
 o = {
     exts = 'mp3;wav;ogm;flac;m4a;wma;ogg;opus;alac;mka',
-    profile = "music"
+    profile = "music",
+
+    undo_profile = ""
 }
 
 opt.read_options(o, 'music_mode')
@@ -32,6 +34,7 @@ end
 
 --stores a table of the extensions
 local exts = split(o.exts)
+local inMusicMode = false
 
 function main()
     --finds the filename
@@ -44,10 +47,18 @@ function main()
     msg.verbose('extracted extension: ' .. ext)
 
     --if the extension is a valid audio extension then it switches to music mode
-    if inTable(ext, exts) then
+    if inTable(ext, exts) and inMusicMode == false then
         msg.verbose('extension in whitelist, applying profiles')
         mp.commandv('apply-profile', o.profile)
         mp.commandv('script-message', 'update-osc-options')
+
+        inMusicMode = true
+    elseif inMusicMode then
+        msg.verbose('extension not in whitelist, applying undo profiles')
+        mp.commandv('apply-profile', o.undo_profile)
+        mp.commandv('script-message', 'update-osc-options')
+
+        inMusicMode = false
     end
 end
 
