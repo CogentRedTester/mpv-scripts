a9340a2515a513687684af764af80cb0fd2f1bd9
Oscar Manglaras <cogent.redtester@outlook.com>
2019-12-22 03:09:08 +1030

    actually implemented cycle-commands script

--- cycle-command.lua
+++ cycle-command.lua
@@ -1,99 +1,119 @@
 --[[
-    script to cycle profiles with a keybind, accomplished through script messages
+    script to cycle commands with a keybind, accomplished through script messages
     syntax:
-        script-message cycle-profiles "profile1;profile2;profile3"
+        script-message cycle-commands "commandline1|commandline2|commandline3"
 
-    You must use semicolons to separate the profiles, do not include any spaces that are not part of the profile name.
-    The script will print the profile description to the screen when switching, if there is no profile description, then it just prints the name
+    Everything between | is run like a normal command line in input.conf, this includes using semicolons to run multiple commands at once
+    If you need to send a command with spaces, such as a multi-word string, use '_' in place of the space
+    for example:
+        
+        script-message cycle-commands "show-text one 1000 ;  print-text two | show-text three_four"
+    
+    This would print 'one' to the OSD for 1 second and 'two' to the console on keypress one and 'three four' to the OSD for 3 seconds on keypress two
+
+    There are no limits to the number of commands, and the script message can be used as often as one wants, the script stores the current iteration
+    for each unique cycle command, so there should be no overlap unless one binds the exact same command string (including spacing)
+
+    If you want to change the characters for the seperators use script-opts
 ]]--
 
---change this to change what character separates the profile names
-seperator = ";"
 
 msg = require 'mp.msg'
+utils = require 'mp.utils'
+opt = require 'mp.options'
 
---splits the profiles string into an array of profile names
-function mysplit (inputstr, seperator)
-    local table = {}
+--set the characters to use as seperators
+--setting any of these to letters, numbers, or hyphens is a VERY bad idea
+o = {
+    --seperates whole command lines
+    cycle_seperator = '|',
+
+    --seperates commands
+    command_seperator = ';',
+
+    --seperates words in commands
+    wordSeperator = ' ',
+
+    --replaces spaces in command
+    --necessary if one want to use multi-word strings, and spaces are set as the word seperator (default)
+    replace_space_char = '_'
+}
+
+opt.read_options(o, 'cycle-commands')
+
+--splits the string into an array of strings around the separator
+function splitString(inputstr, seperator)
+    local t = {}
     for str in string.gmatch(inputstr, "([^"..seperator.."]+)") do
-            table.insert(table, str)
+            table.insert(t, str)
     end
-    return table
+    return t
 end
 
---table of all available profiles and options
-profileList = mp.get_property_native('profile-list')
+--keeps track of commands and iterators
+commands = {}
+iterators = {}
 
---keeps track of current profile for every unique cycle
-iterator = {}
+--[[
+the script stores the command in a 4D table
+the table is in the format:
+    table[full string][strings between '|'][strings between ';'][strings between ' ']
+Or alternatively:
+    table[full string][cycle][command][word]
 
---stores descriptions for profiles
---once requested a description is stored here so it does not need to be found again
-profilesDescs = {}
+examples: commands['show-text hello | show-text bye'][2][1][2] = 'bye'
+          commands['show-text one ; show-text two | show-text three'][1][2][2] = 'two'
+]]--
+function main(str)
 
---if trying to cycle to an unknown profile this function is run to find a description to print
-function findDesc(profile)
-    msg.verbose('unknown profile ' .. profile .. ', searching for description')
+    --if there is nothing saved for the current string, then runs through the process of storing the commands in the table
+    if commands[str] == nil then
+        iterators[str] = 1
 
-    for i = 1, #profileList, 1 do
-        if profileList[i]['name'] == profile then
-            msg.verbose('profile found')
-            local desc = profileList[i]['profile-desc']
-            
-            if desc ~= nil then
-                msg.verbose('description found')
-                profilesDescs[profile] = desc
-            else
-                msg.verbose('no description, will use name')
-                profilesDescs[profile] = profile
+        msg.verbose('unknown cycle, creating command table')
+
+        --splits each cycle around the character '|'
+        local t = splitString(str, o.cycle_seperator)
+        commands[str] = t
+        msg.debug(utils.to_string(t))
+
+        --splits each command in each cycle around ';'
+        for i=1, #commands[str], 1 do
+            local t = splitString(commands[str][i], o.command_seperator)
+            msg.debug(utils.to_string(t))
+            commands[str][i] = t
+
+            --splits each word in each command around ' '
+            for j=1, #commands[str][i], 1 do
+                local t = splitString(commands[str][i][j], o.wordSeperator)
+                msg.debug(utils.to_string(t))
+                commands[str][i][j] = t
+
+                --changes all alternate space characters to spaces
+                for k=1, #t, 1 do
+                    commands[str][i][j][k] = string.gsub(commands[str][i][j][k], o.replace_space_char, ' ')
+                end
             end
-            return
         end
+
+        msg.verbose('command table complete')
     end
 
-    msg.verbose('profile not found')
-    profilesDescs[profile] = "no profile '" .. profile .. "'"
-end
+    local i = iterators[str]
 
---prints the profile description to the OSD
---if the profile has not been requested before during the session then it runs findDesc()
-function printProfileDesc(profile)
-    local desc = profilesDescs[profile]
-    if desc == nil then
-        findDesc(profile)
-        desc = profilesDescs[profile]
+    --runs each command in that cycle
+    for j=1, #commands[str][i], 1 do
+
+        --sends command native the array of words in the command
+        mp.command_native(commands[str][i][j])
     end
 
-    msg.verbose('profile description: ' .. desc)
-    mp.osd_message(desc)
-end
-
-function main(profileStr)
-    --if there is not already an iterator for this cycle then it creates one
-    if iterator[profileStr] == nil then
-        msg.verbose('unknown cycle, creating new iterator')
-        iterator[profileStr] = 1
-    end
-    local i = iterator[profileStr]
-
-    --converts the string into an array of profile names
-    local profiles = mysplit(profileStr, seperator)
-    msg.verbose('cycling ' .. tostring(profiles))
-    msg.verbose("number of profiles: " .. tostring(#profiles))
-
-    --sends the command to apply the profile
-    msg.info("applying profile " .. profiles[i])
-    mp.commandv('apply-profile', profiles[i])
-
-    --prints the profile description to the OSD
-    printProfileDesc(profiles[i])
-
-    --moves the iterator
-    iterator[profileStr] = iterator[profileStr] + 1
-    if iterator[profileStr] > #profiles then
+    --moves the iterator forward
+    iterators[str] = iterators[str] + 1
+    if iterators[str] > #commands[str] then
         msg.verbose('reached end of profiles, wrapping back to start')
-        iterator[profileStr] = 1
+        iterators[str] = 1
     end
 end
 
-mp.register_script_message('cycle-profiles', main)
+mp.register_script_message('cycle-commands', main)
