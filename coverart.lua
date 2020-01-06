00b379973c344c0c3b76577a3730d95673b1a9d9
CogentRedTester <cogent.redtester@outlook.com>
2020-01-06 18:39:56 +1030

    coverart: filename whitelist now only needs the name, not the extension. ImageExts options now provides the extension

--- coverart.lua
+++ coverart.lua
@@ -6,23 +6,30 @@ msg = require 'mp.msg'
 opt = require 'mp.options'
 
 local o = {
-    --list of names of valid cover art, must be separated by semicolon with no spaces
+    --list of names of valid cover art, must be separated by semicolons with no spaces
     --the script is not case specific
-    filenames = "cover.jpg;cover.png;folder.jpg;folder.png;album.jpg;album.png",
+    --any file with valid filenames and valid image extensions are loaded
+    filenames = "cover;folder;album;front",
 
     --ignore the filename and load all image files
     load_all = false,
 
-    --valid image extensions, same rules as the filenames option applies
+    --valid image extensions, same syntax as the filenames option
+    --leaving it blank will load files of any type (with the matching filename)
     imageExts = 'jpg;jpeg;png;bmp;gif',
 
-    --only loads cover art when playing files with the following extensions:
+    --only loads cover art when playing files with these extensions
+    --setting it to blank will cause the script to treat all files as audio files
     audioExts = 'mp3;wav;ogm;flac;m4a;wma;ogg;opus;alac;mka;aiff',
 
     --file path of a placeholder image to use if no cover art is found
     --will only be used if force-window is enabled
     --leaving it blank will be the same as disabling it
-    placeholder = ""
+    placeholder = "",
+
+    --load the placeholder for all files, not just ones that match the audio extension whitelist
+    --still only activates the placeholder when force-window is enabled
+    always_use_placeholder = true
 }
 
 local filenames = {}
@@ -61,8 +68,11 @@ function processStrings()
     audioExts = split(o.audioExts)
 end
 
+--loads a placeholder image as cover art for the file
 function loadPlaceholder()
     if o.placeholder == "" then return end
+
+    if not (mp.get_property('vid') == "no" and mp.get_property_bool('force-window')) then return end
 
     msg.verbose('file does not have video track, loading placeholder')
     local path = mp.command_native({"expand-path", o.placeholder})
@@ -81,10 +91,10 @@ function checkForCoverart()
     --does not look for cover art if the file does not have a valid extension
     local ext = filepath:sub(filepath:find([[.[^.]*$]]) + 1)
     msg.verbose('file extension: ' .. ext)
-    if inTable(ext, audioExts) == false then
+    if o.audioExts ~= "" and inTable(ext, audioExts) == false then
         msg.verbose('file does not have valid extension, aborting coverart search')
         --loads a placeholder image if no covers were found and a window is forced
-        if mp.get_property('vid') == "no" and mp.get_property_bool('force-window') then
+        if o.always_use_placeholder then
             loadPlaceholder()
         end
         return
@@ -111,18 +121,24 @@ function checkForCoverart()
     for i = 1, #files, 1 do
         msg.debug('found file: ' .. files[i])
         local file = string.lower(files[i])
-        local fileext
 
-        --extracts the file extension if load_all is enabled
-        if o.load_all then
-            local index = string.find(file, [[.[^.]*$]])
-            msg.debug('index of final period: ' .. index)
-            fileext = file:sub(index + 1)
-            msg.debug('file extension: ' .. fileext)
+        --finds the file extension
+        local index = string.find(file, [[.[^.]*$]])
+        local fileext = file:sub(index + 1)
+
+        --if file extension is not an image then stops processStrings
+        if o.imageExts ~= "" and not inTable(fileext, imageExts) then
+            msg.debug('"' .. fileext .. '" not in whitelist')
+            goto continue
+        else
+            msg.debug('"' .. fileext .. '" in whitelist')
         end
 
+        --sets the file name
+        local filename = file:sub(0, index - 1)
+
         --if the name matches one in the whitelist
-        if inTable(file, filenames) or (o.load_all and inTable(fileext, imageExts)) then
+        if inTable(filename, filenames) or o.load_all then
             msg.verbose(file .. ' found in whitelist - adding as extra video track...')
             local path = utils.join_path(directory, file)
 
@@ -135,12 +151,12 @@ function checkForCoverart()
                 mp.commandv('video-add', path, "auto")
             end
         end
+
+        ::continue::
     end
 
     --loads a placeholder image if no covers were found and a window is forced
-    if mp.get_property('vid') == "no" and mp.get_property_bool('force-window') then
-        loadPlaceholder()
-    end
+    loadPlaceholder()
 end
 
 opt.read_options(o, 'coverart')
