8cf2ec5c16df922bc003b8ee32dfdae0c9bec766
CogentRedTester <cogent.redtester@outlook.com>
2020-04-02 15:51:37 +1030

    ftp-opts.lua: attempts to load a playlist file when loading a directory
    the playlist file is assumed to be inside the directory loaded

--- ftp-opts.lua
+++ ftp-opts.lua
@@ -2,26 +2,28 @@
     This script dynamically changes some settings at runtime when playing files over te ftp protocol
 
     Settings currently changed:
+
         - converts filepaths taken directly from a browser into a string format readable by mpv
             -e.g. "ftp://test%20ing" would become "ftp://test ing"
-        - ordered chapters are loaded from a playlist file in the source directory
+
+        - detects when ftp subtitle files are incorrrectly loaded and attempts to re-add them using the corrected filepath
+
+        - if a directory is loaded it attempts to open a playlist file inside it (default is playlist.pls)
+        
+        - ordered chapters are loaded from a playlist file in the source directory (default is playlist.pls)
 ]]--
 
 local opt = require 'mp.options'
 local msg = require 'mp.msg'
 
 local o = {
-    ftp = false,
-    --directory_playlist = 'playlist.pls',
+    directory_playlist = 'playlist.pls',
     ordered_chapter_playlist = 'playlist.pls',
-    
-    --tests for the ftp protocol by looking at the start of the path string
-    --is not necessary if using an auto-profile (protocol.ftp) to set the above ftp option
-    test_protocol = true,
 }
 
 opt.read_options(o, 'ftp-opts', function() msg.debug('options updated') end)
 
+local ftp = false
 local path
 local replacingFile = false
 
@@ -40,15 +42,28 @@ do
 end
 
 function setFTPOpts()
+    msg.verbose('FTP protocol detected')
+    ftp = true
     path = mp.get_property('path')
 
-    msg.verbose('FTP protocol detected')
+    --converts the path into a valid string
     path = path:gsub([[\]],[[/]])
     path = decodeURI(path)
 
     local directory = path:sub(1, path:find("/[^/]*$"))
-    --local filename = path:sub(path:find("/[^/]*$") + 1)
+    local filename = path:sub(path:find("/[^/]*$") + 1)
     local playlist = directory .. 'playlist.pls'
+
+    --if there is no period in the filename then the file is actually a directory
+    if not filename:find('%.') then
+        msg.verbose('directory loaded - attempting to load playlist file')
+        --if the filename is nill, then the path ends with a /
+        if filename == nil then
+            path = path .. o.directory_playlist
+        else
+            path = path .. "/" .. o.directory_playlist
+        end
+    end
 
     --reloads the file, replacing the old one, 
     if path ~= mp.get_property('path') then
@@ -61,28 +76,72 @@ function setFTPOpts()
     mp.set_property('ordered-chapters-files', playlist)
 end
 
+--tests if the file being opened uses the ftp protocol
 function testFTP()
+    --reloading a file with corrected addresses causes this function to be rerun.
+    --this check prevents the function from being run twice for each file
     if replacingFile then
         replacingFile = false
         msg.verbose('skipping ftp configuration because script reloaded same file')
         return
     end
 
-    if o.ftp then
-        setFTPOpts()
-        return
-    end
-
-    if not o.test_protocol then return end
+    ftp = false
     msg.verbose('checking for ftp protocol')
     path = mp.get_property('path')
     msg.verbose("got past here")
     local protocol = path:sub(1, 3)
     if protocol == "ftp" then
-        o.ftp = true
         setFTPOpts()
     end
 
 end
+
+local prevSub
+local subChanging = false
+
+--converts the URL of an errored subtitle and tries adding it again
+function addSubtitle(sub)
+    --removing the main error message
+    sub = sub:gsub("Can not open external file ", "")
+
+    --removing the space and period at the end of the message
+    sub = sub:sub(1, -3)
+    sub = decodeURI(sub)
+
+    --if this sub was the same as the prev and the addition was not successful, then cancel the function
+    --otherwise this would cause an infinite loop
+    if (sub == prevSub) and subChanging then
+        print(subChanging)
+        msg.verbose('revised sub file was still not valid - cancelling event loop')
+        return
+    end
+    mp.commandv('sub-add', sub)
+    subChanging = true
+    prevSub = sub
+end
+
+--only passes the warning if it matches the desired format
+function parseMessage(event)
+    if not ftp then return end
+
+    local error = event.text
+    if error:find("Can not open external file ") then
+        addSubtitle(error)
+    end
+end
+
+--tracks if the subtitle addition was successful
+function trackChange()
+
+    subChanging = false
+    msg.verbose('track changed')
+end
+
+mp.observe_property('track-list', nil, trackChange)
+
+--scans warning messages to tell if a subtitle track was incorrectly added
+mp.enable_messages('warn')
+mp.register_event('log-message', parseMessage)
 
 mp.register_event('start-file', testFTP)
