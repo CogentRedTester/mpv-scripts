a9f9bbed4c236f34f61e5ce07ea078866ae6b1ad
CogentRedTester <cogent.redtester@outlook.com>
2020-04-09 17:55:11 +0930

    syncplay: added handling for playlist-loop option
    This setting breaks the playlist handling that this script does,
    so I've set this script to automatically revert any
    changes someone tries to make to the option.
    May not be the best solution, but it stops music-mode.lua
    from breaking compatability

--- syncplay.lua
+++ syncplay.lua
@@ -1,23 +1,56 @@
---a script to allow for local playlists to be played in syncplay
+--[[
+    a script to modify mpv behaviour to work best with syncplay, currently just handles local playlists
+
+    Current features:
+
+        - detects when a playlist entry has finished and seeks to time 0:00:00 before skipping to the next playlist entry
+        
+        - since the loop-playlist option breaks the above this script automatically disables the default loop-playlist behaviour
+          and handles playlist looping itself. The behaviour currently replicates the "inf" setting
+
+    set --script-opts=syncplay-enable=yes to enable this script
+    this script cannot be disabled once it has been activated
+]]--
 
 local opt = require 'mp.options'
 local msg = require 'mp.msg'
 
 local o = {
-    enable = false
+    enable = false,
 }
 
-opt.read_options(o, 'syncplay')
+opt.read_options(o, 'syncplay', function() enable() end)
 
 --sets some options when the player starts
-function set_opts()
+local function set_opts()
     mp.set_property('keep-open', 'always')
     mp.set_property_bool('keep-open-pause', false)
     mp.set_property_bool('loop-playlist', false)
 end
 
+local function parse_loop(name, pref)
+    msg.warn('default ' .. name .. ' handling is disabled, see script settings')
+    mp.commandv('show-text', 'default ' .. name .. ' handling is disabled, see script settings', 3000)
+
+    if pref == "no" then
+        return
+    end
+    mp.set_property('loop-playlist', 'no')
+end
+
+local function playlist_next()
+    local current_pos = mp.get_property_number('playlist-pos')
+    local length = mp.get_property_number('playlist-count')
+
+    if (current_pos + 1 == length) and o.inf_loop then
+        mp.set_property_number('playlist-pos', 0)
+    else
+        mp.command('playlist-next')
+    end
+end
+
 --handles moving to next playlist entries
-function reset_time(name, eof)
+local function reset_time(name, eof)
     msg.debug('eof = ' .. tostring(eof))
 
     if eof then
@@ -26,18 +59,23 @@ function reset_time(name, eof)
         local time = mp.get_time()
         while time + 2 > mp.get_time() do end
         mp.set_property_bool('pause', true)
-        mp.set_property_number('time-pos', 0)
+        mp.set_property_number('percent-pos', 0)
 
         while time + 3 > mp.get_time() do end
-        mp.command('playlist-next')
+        playlist_next()
 
         while time+4 > mp.get_time() do end
         mp.set_property_bool('pause', false)
     end
 end
 
-if (o.enable) then
-    msg.info('using syncplay settings')
-    set_opts()
-    mp.observe_property('eof-reached', "bool", reset_time)
+function enable()
+    if (o.enable) then
+        msg.info('using syncplay settings')
+        set_opts()
+        mp.observe_property('eof-reached', "bool", reset_time)
+        mp.observe_property('loop-playlist', "string", parse_loop)
+    end
 end
+
+enable()
