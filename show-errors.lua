75a0ec340fa0ae1619fd43e29d8d1b9d70ca3a07
CogentRedTester <cogent.redtester@outlook.com>
2020-05-03 00:53:04 +0930

    show-errors: darkened text colour

--- show-errors.lua
+++ show-errors.lua
@@ -2,7 +2,7 @@ local ov = mp.create_osd_overlay("ass-events")
 
 mp.enable_messages('error')
 mp.register_event('log-message', function(log)
-    message = "{\\c&H0000CC>&}[" .. log.prefix .. "] " .. log.text
+    message = "{\\c&H0000AA>&}[" .. log.prefix .. "] " .. log.text
     ov.data = ov.data .. message
     ov:update()
 
