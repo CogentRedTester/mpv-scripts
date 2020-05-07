fa9811e46e4d61dfcbd4712a6814f47f61599334
CogentRedTester <cogent.redtester@outlook.com>
2020-05-07 12:38:59 +0930

    profile-command: now works with multiple commands at once

--- profile-command.lua
+++ profile-command.lua
@@ -1,5 +1,14 @@
---an extremely basic script to allow people to send input command via profiles
---currently it can only send one at a time
+--[=====[
+    An extremely basic script to allow people to send input command via profiles
+
+    Cannot send the same command multiple times in a row
+
+    syntax:
+        single command:         ["command","arg1","arg2"]
+        multiple commands:      [["command1","arg"],["command2","arg2"]]
+
+        if you want to include spaces use %20
+]=====]--
 
 local o = {
     cmd = ""
@@ -10,7 +19,24 @@ local utils = require 'mp.utils'
 
 opt.read_options(o, 'profile_command', function()
     if o.cmd == "" then return end
-
+    o.cmd = o.cmd:gsub("%%20", " ")
     local command = utils.parse_json(o.cmd)
-    mp.command_native(command)
+
+    if command == nil then
+        msg.error('invalid command syntax for ' .. o.cmd)
+        return
+    elseif type(command[1]) ~= "table" then
+        local def, error = mp.command_native(command, true)
+        if def then
+            msg.error('Error occurred for command: ' .. utils.to_string(command))
+        end
+        return
+    end
+
+    for i,cmd in ipairs(command) do
+        local def, error = mp.command_native(cmd, true)
+        if def then
+            msg.error('Error occurred for command: ' .. utils.to_string(cmd))
+        end
+    end
 end)
