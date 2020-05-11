97a1037486a5d11a5da94c2d485f3be479e6f6ff
CogentRedTester <cogent.redtester@outlook.com>
2020-05-11 11:15:45 +0930

    command-timeout: added global timeout
    also changed how input is parsed

--- command-timeout.lua
+++ command-timeout.lua
@@ -1,12 +1,13 @@
 --[[
     This script sends an input command, but only after the specified delay
-    The first argument is the time in seconds before the command executes,
-    the following argument if the command in json array format
+    The last argument is the time in seconds before the command executes,
+    the preceding arguments are the command in json array format
 
     example:
-        script-message command-timeout 2 ["show-text" , "hello" , "2000"]
+        script-message command-timeout ["show-text" , "hello" , "2000"] 2
 
-    The timeout is mandatory. The command string can have spaces.
+    If the timeout is not given then it uses the global default set via script-opts
+    The command string can have spaces at any point but before/after the square brackets.
     If the same timeout command is sent before the timer runs out, then the
     timer is reset without sending the command.
 ]]--
@@ -14,39 +15,53 @@
 local mp = require 'mp'
 local msg = require 'mp.msg'
 local utils = require 'mp.utils'
+local opt = require 'mp.options'
+
+local o = {
+    timeout = 5
+}
+opt.read_options(o, 'command_timeout')
 
 local timers = {}
 
 mp.register_script_message('command-timeout', function(...)
     msg.verbose('recieved arguments: ' .. ...)
     local args = {...}
-    if #args < 2 then
+    if #args < 1 then
         msg.error('not enough arguments, script requires at least 2')
-    end
-    local timeout = tonumber(args[1])
-
-    if timeout == nil then
-        msg.error('did not recieve a valid timeout')
     end
 
     local str = ""
-    for i = 2, #args do
+    for i = 1, #args do
         str = str .. " " .. args[i]
     end
 
-    if timers[str] == nil then
-        local cmd = utils.parse_json(str)
-        timers[str] = mp.add_timeout(timeout, function()
-            msg.verbose('sending command: ' .. str)
-            local def, error = mp.command_native(cmd, true)
-
-            if def then
-                msg.error('error sending command ' .. str)
-                msg.error(error)
-            end
-        end)
-    else
+    if timers[str] then
+        msg.verbose('resetting time on timeout: ' .. str)
         timers[str]:kill()
         timers[str]:resume()
+        return
     end
+
+    local cmd, error, timeout = utils.parse_json(str, true)
+    if timeout == "" then
+        msg.verbose('did not recieve timeout, using default')
+        timeout = o.timeout
+    else
+        timeout = tonumber(timeout)
+    end
+    if timeout == nil then
+        msg.error('did not recieve vaid timeout')
+        return
+    end
+
+    timers[str] = mp.add_timeout(timeout, function()
+        msg.verbose('sending command: ' .. str)
+        local def, error = mp.command_native(cmd, true)
+
+        if def then
+            msg.error('error sending command ' .. str)
+            msg.error(error)
+        end
+    end)
 end)
