a5192d57a0fc5bca1ebe65563c73d17959daeb11
Oscar Manglaras <cogent.redtester@outlook.com>
2020-05-29 21:24:21 +0930

    search-commands: improved argument formatting
    Added jumplist for commands, you can now use keys 1-9 to
    import one of the first 9 commands into console.lua

--- search-commands.lua
+++ search-commands.lua
@@ -3,24 +3,56 @@
     The search is case insensitive, and searches the command name.
     The script sends the filter directly to a lua string match function, so you can use patterns to get more complex filtering
 
+    When the search results are up you can auto load the first 9 commands in console.lua by using the number keys
+
     The command is: script-binding search-commands
 
     One the command is sent the console will open with a pre-entered search command, simply add a query string as the first argument.
     Once the results page is displayed it can be dismissed with esc
+
+    Green commands are classified as optional, while cyan are not. However, this does not always seem to be accurate.
 ]]--
 
 local mp = require 'mp'
-local utils = require 'mp.utils'
+local opt = require 'mp.options'
 local msg = require 'mp.msg'
+
+local o = {
+    ass_header = "{\\c&H00ccff>&\\fs40\b900\\q2}",
+    ass_underline = "{\\c&00ccff>&\\fs30\b100}",
+    ass_cmd = "{\\c&Hffccff>\\fs20}",
+    ass_outerbrackets = "{\\fs20\\c&H00cccc>&}",
+    ass_args = "{\\fs20\\c&H33ff66>&}",
+    ass_optargs = "{\fs20\\c&Hffff00>&}",
+    ass_argtype = "{\\c&H00cccc>&}{\\fs12}"
+}
+
+opt.read_options(o, "search_commands")
 
 local ov = mp.create_osd_overlay("ass-events")
 ov.hidden = true
+
+local keys = {
+    "search_commands_key/1",
+    "search_commands_key/2",
+    "search_commands_key/3",
+    "search_commands_key/4",
+    "search_commands_key/5",
+    "search_commands_key/6",
+    "search_commands_key/7",
+    "search_commands_key/8",
+    "search_commands_key/9",
+    "search_commands_key/close_overlay"
+}
 
 function close_overlay()
     ov.data = ""
     ov.hidden = true
     ov:update()
-    mp.remove_key_binding("search_commands_key/close_overlay")
+
+    for _,key in ipairs(keys) do
+        mp.remove_key_binding(key)
+    end
 end
 
 function fix_chars(str)
@@ -30,11 +62,29 @@ function fix_chars(str)
     return str
 end
 
-function add_result(cmd, args)
-    cmd = fix_chars(cmd)
-    args = fix_chars(args)
+--adds the results to the list
+function add_result(cmd, args, num_entry)
+    --adds 1-9 keybinds for the first 9 entries
+    if num_entry < 10 then
+        mp.add_forced_key_binding(tostring(num_entry), keys[num_entry], function()
+            mp.commandv('script-message-to', 'console', 'type', cmd.." ")
+            close_overlay()
+        end)
+    end
 
-    ov.data = ov.data .. "\n" .. "{\\c&Hffff00>&\\fs20}" .. cmd .. "{\\c&H00cccc>&}" .. "      args: " .. "{\\fs15}" .. args .. "\\N \\N"
+    cmd = fix_chars(cmd)
+
+    ov.data = ov.data .. "\n" .. o.ass_cmd .. cmd .. "        "..o.ass_outerbrackets.."("
+    for _,arg in ipairs(args) do
+        if arg.optional then
+            ov.data = ov.data .. o.ass_optargs
+        else
+            ov.data = ov.data .. o.ass_args
+        end
+
+        ov.data = ov.data .. " " .. arg.name .. o.ass_argtype.." ("..arg.type.."), "
+    end
+    ov.data = ov.data .. o.ass_outerbrackets ..") \\N"
 end
 
 function search_keys(keyword)
@@ -42,21 +92,23 @@ function search_keys(keyword)
 
     keyword = keyword:lower()
 
-    ov.data = "{\\c&H00ccff>&\\fs40\b900\\q2}" ..  "Search results for '" .. keyword .. "'\
-    {\\c&00ccff>&\\fs30\b100}-------------------------------------------------------"
+    ov.data = o.ass_header..  "Search results for '" .. keyword .. "'\
+    "..o.ass_underline.."-------------------------------------------------------"
 
+    local num_entries = 0
     for i,command in ipairs(commands) do
         if
         command.name:lower():find(keyword)
         then
+            num_entries = num_entries + 1
             local cmd = command.name
-            add_result(cmd, utils.to_string(command.args))
+            add_result(cmd, command.args, num_entries)
         end
     end
 
     ov.hidden = false
     ov:update()
-    mp.add_forced_key_binding("esc", "search_commands_key/close_overlay", close_overlay)
+    mp.add_forced_key_binding("esc", keys[10], close_overlay)
 end
 
 mp.register_script_message('search_commands/search', function(keyword)
