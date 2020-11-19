e74a772c8ae4631f0e2c9c646a03de4c96bd0683
keiviv <65128541+keiviv@users.noreply.github.com>
2020-11-19 08:27:09 +0200

    Minor button cleanup

--- pause-indicator.lua
+++ pause-indicator.lua
@@ -5,9 +5,9 @@
 
 local mp = require 'mp'
 local ov = mp.create_osd_overlay('ass-events')
-ov.data = [[{\an5\p1\alpha&H79\1c&H0000&\3a&Hff}{\p1}]] ..
+ov.data = [[{\an5\p1\alpha&H79\1c&H0000&\3a&Hff}]] ..
           [[m30 15 b37 8 112 8 117 15 b125 22 125 75 118 82 b112 89 38 89 30 82 b23 75 23 22 30 15 m76 108]] ..
-          [[{\alpha&H10\1c&Hffffff&\3a&Hff} m-45 -24 l 2 2 l -45 28{\p0}]]
+          [[{\alpha&H10\1c&Hffffff&\3a&Hff} m-45 -25 l 2 2 l -45 28{\p0}]]
 
 mp.observe_property('pause', 'bool', function(_, paused)
     mp.add_timeout(0.1, function()
