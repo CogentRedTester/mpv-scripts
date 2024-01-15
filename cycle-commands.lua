1ba8faa37e51bd72d42ef3aa9e985e78ee2f8eaa
CogentRedTester <cogent.redtester@outlook.com>
2024-01-15 17:53:58 +1030

    cycle-commands: improve documentation for command syntax

--- cycle-commands.lua
+++ cycle-commands.lua
@@ -5,25 +5,24 @@
     syntax:
         script-message cycle-commands "command1" "command2" "command3"
 
-    The syntax of each command is identical to the standard input.conf syntax, but each command must be within
-    a pair of double quotes.
+    The syntax of each command is identical to the standard input.conf syntax, but each command must be
+    a single quoted string. Note that this may require you to nest (and potentially escape) quotes,
+    read the mpv documentation for how to do this: https://mpv.io/manual/master/#flat-command-syntax.
 
-    Commands with mutiword arguments require you to send double quotes just like normal command syntax, however,
-    you will need to escape the quotes with a backslash so that they are sent as part of the string.
     Semicolons also work exactly like they do normally, so you can easily send multiple commands each cycle.
 
-    Here is an example of a standard input.conf entry:
-
+    Here are some examples of the same command using different quotes:
         script-message cycle-commands "show-text one 1000 ; print-text two" "show-text \"three four\""
+        script-message cycle-commands 'show-text one 1000 ; print-text two' 'show-text "three four"'
+        script-message cycle-commands ``show-text one 1000 ; print-text two`` ``show-text "three four"``
 
     This would, on keypress one, print 'one' to the OSD for 1 second and 'two' to the console,
     and on keypress two 'three four' would be printed to the OSD.
-    Notice how the quotation marks around 'three four' are escaped using backslashes.
-    All other syntax details should be exactly the same as usual input commands.
+    Note that single (') and backtick (`) quoting was only added in mpv v0.34.
 
     There are no limits to the number of commands, and the script message can be used as often as one wants,
     the script stores the current iteration for each unique cycle command, so there should be no overlap
-    unless one binds the exact same command string (including spacing)
+    unless one binds the exact same command string (including spacing).
 ]=====]--
 
 local mp = require 'mp'
